02b6486c025c8545b8900069bad8b07c97dbb485
MatyasAkos <matyas.akos.23f@szlgbp.hu>
2025-12-14 22:26:30 +0100

    fixed bug with too long messages

--- src/index.js
+++ src/index.js
@@ -103,8 +103,11 @@ client.on('interactionCreate', (interaction) => {
                         result = ''
                         for (let i = 0; i < cf.exams.length; i++) {
                             result += `${i + 1}. ${cf.exams[i].type} in ${cf.exams[i].subject} on ${cf.exams[i].year > new Date().getFullYear() ? `${cf.exams[i].year}.` : ''}${cf.exams[i].month < 9 ? '0' : ''}${cf.exams[i].month + 1}.${cf.exams[i].day < 10 ? '0' : ''}${cf.exams[i].day}.${(cf.exams[i].topic || '') === '' ? '' : ` with the topic of: ${cf.exams[i].topic}`}${cf.exams.length !== i - 1 ? '\n' : ''}`
+                            if(result.length > 1000 || i === cf.exams.length - 1){
+                                interaction.reply(result)
+                                result = ''
+                            }
                         }
-                        interaction.reply(result)
                     }
                 }
                 else if(interaction.commandName === 'remove'){
@@ -194,9 +197,10 @@ async function reminder() {
                         else{
                             j++
                         }
-                    }
-                    if (notify){
-                        client.channels.cache.get(cf.channelid).send(result)
+                        if(result.length > 1000 || (j === cf.exams.length - 1 && notify)){
+                            client.channels.cache.get(cf.channelid).send(result)
+                            result = ''
+                        }
                     }
                     fs.writeFileSync(`${path}/${files[i]}`, JSON.stringify(cf))
                 }
