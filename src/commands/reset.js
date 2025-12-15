aa9042cffc0b73dbf905a48321b6068012486737
MatyasAkos <matyas.akos.23f@szlgbp.hu>
2025-12-15 16:40:06 +0100

    removed code used for debuging

--- src/commands/reset.js
+++ src/commands/reset.js
@@ -1,9 +1,7 @@
 const fs = require('fs')
 
 function reset(interaction, path){
-    /*fs.rmSync(path)
-    interaction.reply('Reset the config')*/
-    interaction.reply('a')
-    interaction.reply('b')
+    fs.rmSync(path)
+    interaction.reply('Reset the config')
 }
 module.exports = {reset}
